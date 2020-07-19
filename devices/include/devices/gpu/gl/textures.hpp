cdbb14090868107998b1b619a9956e5a3088e17e
Łukasz Bogaczyński <woookie41@gmail.com>
2020-07-19 19:00:47 +0200

    Textures filters

--- devices/include/devices/gpu/gl/textures.hpp
+++ devices/include/devices/gpu/gl/textures.hpp
@@ -11,7 +11,15 @@ namespace lucid::gpu
         GLTexture(const GLuint& TextureID, const TextureType& Type, const math::ivec3& Size);
 
         virtual math::ivec3 GetSize() const override;
+
         virtual void Bind() override;
+        virtual void Unbind() override;
+
+        virtual void SetMinFilter(const MinTextureFilter& Filter) override;
+        virtual void SetMagFilter(const MagTextureFilter& Filter) override;
+        virtual void SetWrapSFilter(const WrapTextureFilter& Filter) override;
+        virtual void SetWrapTFilter(const WrapTextureFilter& Filter) override;
+
         virtual ~GLTexture();
 
       private:
--- main.cpp
+++ main.cpp
@@ -24,13 +24,13 @@ int main(int argc, char** argv)
 
     lucid::gpu::Texture* containerTexture = lucid::gpu::CreateTextureFromJPEG("container.jpg");
     lucid::gpu::Texture* faceTexture = lucid::gpu::CreateTextureFromPNG("awesomeface.png");
-    
+
     lucid::math::ivec3 containerTextureSize = containerTexture->GetSize();
     lucid::math::ivec3 faceTextureSize = faceTexture->GetSize();
 
     lucid::canvas::CanvasItem canvasRoot;
     canvasRoot.Position = { 400, 300 };
-    
+
     lucid::canvas::Sprite sprite1;
     sprite1.Position = { 200, 0 };
     sprite1.SpriteSize = { 200, 200 };
@@ -40,7 +40,7 @@ int main(int argc, char** argv)
     lucid::canvas::Sprite sprite2;
     sprite2.Position = { 0, 100 };
     sprite2.SpriteSize = { 200, 200 };
-    sprite2.TextureRegionSize = { faceTextureSize.x, faceTextureSize.y };
+    sprite2.TextureRegionSize = { faceTextureSize.x + 300, faceTextureSize.y };
     sprite2.TextureToUse = faceTexture;
     sprite2.RespectParentPosition = false;
 
@@ -73,21 +73,31 @@ int main(int argc, char** argv)
             break;
         }
 
+        if (lucid::WasKeyPressed(SDLK_d))
+        {
+            faceTexture->Bind();
+            faceTexture->SetWrapSFilter(lucid::gpu::WrapTextureFilter::CLAMP_TO_EDGE);
+            faceTexture->Unbind();
+        }
+
         if (lucid::WasKeyPressed(SDLK_a))
         {
-            canvasRoot.IsVisible = !canvasRoot.IsVisible;
+            faceTexture->Bind();
+            faceTexture->SetWrapSFilter(lucid::gpu::WrapTextureFilter::REPEAT);
+            faceTexture->Unbind();
         }
 
         if (lucid::IsMouseButtonPressed(lucid::MouseButton::LEFT))
         {
-            sprite2.Position = {(float)lucid::GetMousePostion().x, (float) window->GetSize().y -  lucid::GetMousePostion().y};
+            sprite2.Position = { (float)lucid::GetMousePostion().x,
+                                 (float)window->GetSize().y - lucid::GetMousePostion().y };
         }
-
 
         if (lucid::WasMouseButtonPressed(lucid::MouseButton::RIGHT))
         {
-            sprite2.Position = {0, 0};
+            sprite2.Position = { 0, 0 };
         }
+
 
         window->ClearColor();
         canvasRoot.Draw(simpleShader);
